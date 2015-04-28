bbd04c4b0cd1251294a9fc39e5e2e6c2a0f60090
Mike Elsmore <mike@elsmore.me>
2015-04-28 13:25:39 -0400

    Add config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes       = require('./routes/index');
+var users        = require('./routes/users');
+
+var configs      = require('./config.js');
 
 var app = express();
 
